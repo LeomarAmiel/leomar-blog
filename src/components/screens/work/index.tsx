c8a49c0796023e5ec038d2dc4bc504c8d2a1669f
leomaramiel <leomaramieldev@gmail.com>
2020-09-29 15:04:46 +0800

    Update changes to work

--- src/components/screens/work/index.tsx
+++ src/components/screens/work/index.tsx
@@ -8,15 +8,70 @@ const Heading4 = styled(Paragraph)`
   margin-bottom: 1.6rem;
 `;
 
+const ListItemHeading = styled.h5`
+  font-size: 1.7rem;
+  color: ${(p) => p.theme.text};
+`;
+
+const ListItemParagraph = styled.p`
+  font-size: 1.4rem;
+  color: ${(p) => p.theme.text};
+`;
+
+const ListItemDate = styled.p`
+  font-size: 1rem;
+  color: ${(p) => p.theme.inactiveTabText};
+`;
+
 const Work: FC<unknown> = () => {
   return (
     <S.Section>
-      <Heading4>Wear your helmet! ⛑</Heading4>
-
-      <Paragraph>
-        🚧 This part is of my website is still <b>in progress</b>. Steer clear
-        of the area! 🚧
-      </Paragraph>
+      <S.UnorderedList>
+        {/*  make it some kind of mapping of an array */}
+        <S.ListItem>
+          <ListItemHeading>CodeX Software Solutions</ListItemHeading>
+          <ListItemDate>April 2020 - Present</ListItemDate>
+          <ListItemParagraph>
+            Working for a fintech company which specializes in wellbeing and
+            creating an insurance journey.
+          </ListItemParagraph>
+        </S.ListItem>
+        <S.ListItem>
+          <ListItemHeading>YuLife</ListItemHeading>
+          <ListItemDate>March 2019 - March 2020</ListItemDate>
+          <ListItemParagraph>
+            At the forefront of modernizing insurance by transforming
+            traditional insurance products into life-enhancing experiences
+            powered by the gamified wellbeing app.
+          </ListItemParagraph>
+        </S.ListItem>
+        <S.ListItem>
+          <ListItemHeading>Playbook</ListItemHeading>
+          <ListItemDate>Jan 2019 - Feb 2020</ListItemDate>
+          <ListItemParagraph>
+            Playbook is the marketplace where you can find your favorite
+            trainers, coaches and athletes where they can share their workouts
+            and coach you.
+          </ListItemParagraph>
+        </S.ListItem>
+        <S.ListItem>
+          <ListItemHeading>PDAX</ListItemHeading>
+          <ListItemDate>May 2018 - Jan 2019</ListItemDate>
+          <ListItemParagraph>
+            The first order-book exchange that gives Filipinos the opportunity
+            to invest and trade digital assets and cryptocurrencies.
+          </ListItemParagraph>
+        </S.ListItem>
+        <S.ListItem>
+          <ListItemHeading>PDAX</ListItemHeading>
+          <ListItemDate>May 2018 - Jan 2019</ListItemDate>
+          <ListItemParagraph>
+            Jumped from project o proejct, from doing mail migration to
+            supporting a project management tool used by Accenture employees
+            around the world.
+          </ListItemParagraph>
+        </S.ListItem>
+      </S.UnorderedList>
     </S.Section>
   );
 };
--- src/components/screens/work/work.styles.ts
+++ src/components/screens/work/work.styles.ts
@@ -2,4 +2,14 @@ import styled from "styled-components";
 
 export const Section = styled.section`
   padding: 0 20%;
+  @media (max-width: 600px) {
+    padding: 0 2rem;
+  }
+`;
+
+export const UnorderedList = styled.ul``;
+
+export const ListItem = styled.li`
+  color: ${(p) => p.theme.text};
+  margin: 2.4rem 0;
 `;
