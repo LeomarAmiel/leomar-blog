5c65f861974b197c80ce7e638dd836f55b8d176f
leomaramiel <leomaramieldev@gmail.com>
2020-10-04 23:43:32 +0800

    Bugfix: Sitemap xml

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,19 +1,18 @@
 import { NextPage } from "next";
 
-const initialSitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>https://leomaramiel.tech</loc>
-            <lastmod>2020-10-04</lastmod>
-            <changefreq>weekly</changefreq>
-        </url>
-        <url>
-            <loc>https://leomaramiel.tech/work</loc>
-            <lastmod>2020-10-04</lastmod>
-            <changefreq>weekly</changefreq>
-        </url>
-    </urlset>
+const initialSitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://leomaramiel.tech</loc>
+        <lastmod>2020-10-04</lastmod>
+        <changefreq>weekly</changefreq>
+    </url>
+    <url>
+        <loc>https://leomaramiel.tech/work</loc>
+        <lastmod>2020-10-04</lastmod>
+        <changefreq>weekly</changefreq>
+    </url>
+</urlset>
 `;
 
 const Sitemap: NextPage<unknown> = () => null;
