4e5c28d85d6c086ef0ecf34f0027e3a5c8768b6b
leomaramiel <leomaramieldev@gmail.com>
2020-10-05 00:38:31 +0800

    Try hack from github

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,25 +1,24 @@
 import { GetServerSidePropsContext } from "next";
 
-const initialSitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://leomaramiel.tech</loc>
-        <lastmod>2020-10-04</lastmod>
-        <changefreq>weekly</changefreq>
-    </url>
-    <url>
-        <loc>https://leomaramiel.tech/work</loc>
-        <lastmod>2020-10-04</lastmod>
-        <changefreq>weekly</changefreq>
-    </url>
-</urlset>
+const initialSitemap = `<?xml version="1.0" encoding="utf-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://leomaramiel.tech</loc>
+            <lastmod>2020-10-04</lastmod>
+            <changefreq>weekly</changefreq>
+        </url>
+        <url>
+            <loc>https://leomaramiel.tech/work</loc>
+            <lastmod>2020-10-04</lastmod>
+            <changefreq>weekly</changefreq>
+        </url>
+    </urlset>
 `;
 
-const Sitemap = () => null;
-
 export async function getServerSideProps({ res }: GetServerSidePropsContext) {
-  res?.setHeader("content-type", "application/xml");
-  res?.end(initialSitemap);
+  res.setHeader("Content-Type", "text/xml");
+  res.write(initialSitemap);
+  res.end();
 }
 
-export default Sitemap;
+export default () => null;
